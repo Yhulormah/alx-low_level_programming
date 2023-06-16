dc4e7b5b0ac84bcefdd200326565ed1277c088d3
yhulormah <nwosuulomaa@gmail.com>
2023-06-15 19:14:27 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main - Entry
- * Return: Always
+ * *Description:main-prints out a string to stdout
+ * *Return:0 if success
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art, /n")
-	return (0) :
+printf("with proper grammar, but the outcome is a piece of art,/n");
+	return (0);
