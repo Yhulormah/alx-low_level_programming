e7d57f210912b0e07ff7985832bf76731b768bd8
yhulormah <nwosuulomaa@gmail.com>
2023-06-20 20:18:18 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - compute the value of an interger
+ * @n: check the int
+ * Return: the value of int
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
+
